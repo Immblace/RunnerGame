cd4fe8e0e625525a37899fd73e394abd79a67d5f
Farmagood <Immblace@gmail.com>
2025-01-22 17:18:04 +0400

    update assets and other fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,167 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Vector3 startPos;
+    [SerializeField] private float speed;
+    [SerializeField] private Transform cameraPos;
+    [SerializeField] private Transform CameraLookPos;
+    private LevelManager levelManager;
+    private SpawnGenerate spawnGenerate;
+    private ScoreManager scoreManager;
+    private bool moveRight;
+    private bool moveLeft;
+    private int count = 0;
+    private Vector3 touchStartPos;
+    private bool isGrounded;
+    private Animator anim;
+    private Rigidbody _rb;
+    private float timeToUpSpeed = 3f;
 
 
-    private void Update()
+    private void Start()
     {
-        MoveMouse();
+        spawnGenerate = FindObjectOfType<SpawnGenerate>();
+        levelManager = FindObjectOfType<LevelManager>();
+        scoreManager = FindObjectOfType<ScoreManager>();
+        anim = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        if (timeToUpSpeed > 0)
+        {
+            timeToUpSpeed -= Time.deltaTime;
+        }
+        else
+        {
+            speed += 0.6f;
+            timeToUpSpeed = UnityEngine.Random.Range(5, 9);
+        }
+
         SwipeMove();
+        
+
+
+        if (moveRight && count == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                transform.Rotate(Vector3.up, 90f);
+                count++;
+            }
+        }
+
+        if (moveLeft && count == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.Rotate(Vector3.up, -90f);
+                count++;
+            }
+        }
+
+        if (transform.position.y < -20f)
+        {
+            scoreManager.CheckRecord();
+            levelManager.onLoseMenu();
+            transform.position = new Vector3(0f, 0.4f, -25f);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+        cameraPos.position = Vector3.Lerp(cameraPos.position, transform.TransformPoint(0, 3.5f, -3.3f), 5f * Time.fixedDeltaTime);
+        cameraPos.LookAt(CameraLookPos);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Block")
+        {
+            scoreManager.CheckRecord();
+            levelManager.onLoseMenu();
+        }
+
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            anim.SetInteger("State", 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Right")
+        {
+            moveRight = true;
+        }
+
+        if (other.tag == "Left")
+        {
+            moveLeft = true;
+        }
+
+        if (other.tag == "Bullet")
+        {
+            scoreManager.CheckRecord();
+            levelManager.onLoseMenu();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Right" || other.tag == "Left")
+        {
+            spawnGenerate.DeletObj();
+            moveLeft = false;
+            moveRight = false;
+            count = 0;
+        }
+    }
+
+    private void Jump()
+    {
+        _rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+        anim.SetInteger("State", 1);
+    }
+
+    private void Move()
+    {
+        _rb.position += transform.forward * speed * Time.fixedDeltaTime;
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rb.position += transform.right * 3f * Time.fixedDeltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rb.position += transform.right * -3f * Time.fixedDeltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
     }
 
     private void SwipeMove()
@@ -22,66 +173,41 @@ public class Player : MonoBehaviour
             switch (touch.phase)
             {
                 case TouchPhase.Began:
-                    startPos = touch.position;
+                    touchStartPos = touch.position;
                     break;
-                case TouchPhase.Canceled:
-                    float deltaX = touch.position.x - startPos.x;
-
-                    if (Mathf.Abs(deltaX) > 5f)
+                case TouchPhase.Ended:
+                    Vector3 delta = (Vector3)touch.position - touchStartPos;
+                    
+                    if (delta.magnitude > 200f)
                     {
-                        if (deltaX > 0)
+                        if (Math.Abs(delta.x) > Math.Abs(delta.y))
                         {
-                            if (transform.position.x < 1.75f)
+                            if (delta.x > 0 && moveRight && count == 0)
                             {
-                                transform.position += Vector3.right * 1.8f;
+                                transform.Rotate(Vector3.up, 90f);
+                                count++;
+                            }
+
+                            if (delta.x < 0 && moveLeft && count == 0)
+                            {
+                                transform.Rotate(Vector3.up, -90f);
+                                count++;
                             }
                         }
                         else
                         {
-                            if (transform.position.x > -1.75f)
+                            if (delta.y > 0 && isGrounded)
                             {
-                                transform.position += Vector3.left * 1.8f;
+                                Jump();
                             }
                         }
                     }
 
-                    startPos = Vector3.zero;
+
+                    touchStartPos = Vector3.zero;
                     break;
             }
         }
     }
 
-
-    private void MoveMouse()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            startPos = Input.mousePosition;
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            float deltaX = Input.mousePosition.x - startPos.x;
-
-            if (Mathf.Abs(deltaX) > 5f)
-            {
-                if (deltaX > 0)
-                {
-                    if (transform.position.x < 1.75f)
-                    {
-                        transform.position += Vector3.right * 1.8f;
-                    }
-                }
-                else
-                {
-                    if (transform.position.x > -1.75f)
-                    {
-                        transform.position += Vector3.left * 1.8f;
-                    }
-                }
-            }
-
-            startPos = Vector3.zero;
-        }
-    }
 }
